8ed3a13d63075198c00f91d6c5e664edb2c88ebf
Emiliano2152 <emilianotorrejon@gmail.com>
2026-02-04 17:30:29 -0300

    Add CORS support and create frontend HTML file with fetch example

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,21 @@
 import express, { type Request, type Response } from 'express';
 import { archivoHtml } from './index-de-emi';
+import cors from 'cors';
 
 const app = express();
 const puerto = 3223;
+
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript Express!');
 });
 
-app.get('/ping', (req: Request, res: Response) => {
+// Adds headers: Access-Control-Allow-Origin: *
+app.use(cors());
+
+app.get('/ping', async (req: Request, res: Response) => {
+  await delay(2000);
   res.send('Pong');
 });
 
