9b33bfc8921e9e186518a10b2241f49fcf98cf28
Emiliano Torrejon <emilianotorrejon@gmail.com>
2026-02-18 20:30:37 +0000

    Add CORS support and refactor app structure for improved request handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,21 +1,16 @@
 import express, { type Request, type Response } from 'express';
 import { archivoHtml } from './index-de-emi';
-import cors from 'cors';
 
 const app = express();
-const puerto = 3223;
+const port = 3223;
 
-const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript Express!');
 });
 
-// Adds headers: Access-Control-Allow-Origin: *
-app.use(cors());
-
-app.get('/ping', async (req: Request, res: Response) => {
-  await delay(2000);
+app.get('/ping', (req: Request, res: Response) => {
   res.send('Pong');
 });
 
@@ -23,6 +18,39 @@ app.get('/archivo', (req: Request, res: Response) => {
   res.send(archivoHtml);
 });
 
-app.listen(puerto, () => {
-  console.log(`Server is running at http://localhost:${puerto}`);
+/*
+insomnia -> POST with BODY "rock" / "paper" / "scissors"
+
+POST /rps :
+- validacion del input del usuario
+- crear un resultado de la maquina random
+- compara con el del usuario
+- retorna string "Ganaste" / "Perdiste" / "Empate"
+*/
+
+// Middleware to parse JSON bodies
+
+// Route to handle POST requests to '/src'
+app.post('/rps', (req, res) => {
+  let incomingData = req.body;
+  // Validación
+  if (incomingData['move'] === undefined) {
+    res.status(400).json('Poné bien move culiado');
+    return;
+  }
+  // TODO: validación de que move sea "rock", "paper" o "scisors"
+
+  // Lógica
+  console.log(incomingData['move']);
+
+  // Send a response back to the client, typically with a 201 status code for resource creation
+  res.status(201).json({
+    message: 'Data received successfully',
+    data: incomingData,
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
 });
